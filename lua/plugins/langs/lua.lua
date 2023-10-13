ea8ca6bb32c7d9181c07d5b3e72366dc7cbdbb92
Bendegúz Cseresnyés <cser.bende@gmail.com>
2023-10-13 23:06:45 +0200

    Update lua conf

--- lua/plugins/langs/lua.lua
+++ lua/plugins/langs/lua.lua
@@ -1,25 +1,37 @@
 return {
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-    opts = function(plug, opts)
+    opts = function(_, opts)
       table.insert(opts.ensure_installed, "stylua")
+      table.insert(opts.ensure_installed, "lua_ls")
       return opts
     end,
   },
   {
     'nvim-treesitter/nvim-treesitter',
-    opts = function(plug, opts)
+    opts = function(_, opts)
       table.insert(opts.ensure_installed, "lua")
       return opts
     end,
   },
   {
     'mhartington/formatter.nvim',
-    opts = function(plug, opts)
+    opts = function(_, opts)
       opts.autoformat["lua"] = true
       opts.filetype["lua"] = {
-        { require"formatter.filetypes.lua".stylua }
+        {
+          exe = "stylua"
+        }
       }
+      return opts
+    end,
+  },
+  {
+
+    'neovim/nvim-lspconfig',
+    dependencies = {},
+    opts = function(_, opts)
+      opts.servers.lua_ls = {}
       return opts
     end,
   },

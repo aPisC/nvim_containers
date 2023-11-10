8041d043be82c7eebbdb158445409c908e5cd7c5
Bendeguz Cseresnyes <cser.bende@gmail.com>
2023-11-10 19:45:49 +0100

    Ad dtailwind support

--- lua/dist_init.lua
+++ lua/dist_init.lua
@@ -29,6 +29,7 @@ return function(system_dist_config)
     scala = false,
     typescript = false,
     python = false,
+    tailwind = false,
 
     -- plugins on indexes
   }
@@ -64,6 +65,7 @@ return function(system_dist_config)
       {import="plugins.langs.scala", enabled = dist_config.scala},
       {import="plugins.langs.typescript", enabled = dist_config.typescript},
       {import="plugins.langs.python", enabled = dist_config.python},
+      {import="plugins.langs.tailwind", enabled = dist_config.tailwind},
     }
 
     -- Collect plugins from configs

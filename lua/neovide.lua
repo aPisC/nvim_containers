91af2502822333ad1e52d7a37558e102a0e777d5
Bendegúz Cseresnyés <cser.bende@gmail.com>
2025-05-30 09:51:20 +0200

    Fix terminal paste keymap

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -13,6 +13,7 @@ if vim.g.neovide then
   vim.keymap.set({ "n", "v" }, "<C-ScrollWheelUp>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
   vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
   vim.keymap.set({ "i" }, "<C-S-V>", "<C-o>:set paste<CR><C-R>+<C-o>:set nopaste<CR>")
-  vim.keymap.set({ "t", "c" }, "<C-S-V>", "<C-R>+")
+  vim.keymap.set({ "c" }, "<C-S-V>", "<C-R>+")
+  vim.keymap.set({ "t" }, "<C-S-V>", "<C-\\><C-n>pi")
   vim.keymap.set({ "n", "v" }, "<C-S-N>", function() vim.fn.jobstart("neovide", { detach = true, cwd=vim.env.HOME }) end)
 end
